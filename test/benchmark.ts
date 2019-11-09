3b0905a4182bab43b6b595315ad44e6c5155f523
Emil Laine <laine.emil@gmail.com>
2019-11-09 15:43:23 +0200

    Add more performance tests and improve existing ones

--- test/benchmark.ts
+++ test/benchmark.ts
@@ -1,53 +1,64 @@
 // @ts-ignore
 import benchmark from 'nodemark'
+import { sample } from 'lodash'
 import { runTestMigration } from './test-utils'
-import { Type } from '../src/types'
+import { Database, Type } from '../src/types'
 import { deleteDatabase } from '../src/storage'
 
 async function main() {
-  const db = await runTestMigration(db =>
-    db.createCollection('orders')
-      .orders.addProperty('price', Type.number)
-      .orders.addProperty('discount', Type.number)
-      .orders.addProperty('name', Type.string)
-      .orders.addProperty('description', Type.string)
-      .orders.addProperty('date', Type.string)
-      .orders.addProperty('customer', Type.string)
-      .orders.addProperty('salesperson', Type.string)
-  )
-
   const randomString = () => Math.random().toString(36)
-  const objects = Array.from({ length: 100 }, () => ({
+  const objects = Array.from({ length: 10000 }, () => ({
     price: Math.random() * 5000,
     discount: Math.random() < 0.01 ? 20 : 0,
     name: randomString(),
     description: randomString() + randomString() + randomString(),
     date: new Date().toISOString(),
     customer: randomString(),
-    salesperson: randomString()
+    salesperson: ''
   }))
 
   const tests: Record<string, Function> = {}
 
-  tests.insert = async () => {
-    for (const object of objects) {
-      await db.orders.insert(object).save()
-    }
+  tests.insert = async (db: Database) => {
+    await db.orders.insert(sample(objects)).save()
   }
 
-  tests.insertMany = async () => {
-    await db.orders.insertMany(objects).save()
+  tests.insertMany = async (db: Database) => {
+    await db.orders.insertMany([sample(objects)]).save()
+  }
+
+  tests.update = async (db: Database) => {
+    await db.orders.update({ salesperson: randomString() }).save()
+  }
+
+  tests.findAll = async (db: Database) => {
+    await db.orders.findAll()
+  }
+
+  tests.findAllByProperty = async (db: Database) => {
+    await db.orders.findAll({ customer: 'nonexistent' })
   }
 
   const maxKeyLength = Math.max(...Object.keys(tests).map(key => key.length))
 
   for (const key in tests) {
-    const test = tests[key]
-    const result = await benchmark((done: Function) => test().then(done))
-    console.log(key.padEnd(maxKeyLength), result.toString('milliseconds'))
-  }
+    const db = await runTestMigration(db =>
+      db.createCollection('orders')
+        .orders.addProperty('price', Type.number)
+        .orders.addProperty('discount', Type.number)
+        .orders.addProperty('name', Type.string)
+        .orders.addProperty('description', Type.string)
+        .orders.addProperty('date', Type.string)
+        .orders.addProperty('customer', Type.string)
+        .orders.addProperty('salesperson', Type.string)
+    )
+    await db.orders.insertMany(objects).save()
 
-  await deleteDatabase(db)
+    const test = tests[key]
+    const result = await benchmark((done: Function) => test(db).then(done))
+    console.log(key.padEnd(maxKeyLength), result.toString('milliseconds'))
+    await deleteDatabase(db)
+  }
 }
 
 main()
